0e26bee5520dc80cc0bbf701aaa2ad7729086fc6
umayer10-AI <mdumayer10@gmail.com>
2026-04-23 08:30:07 +0600

    add

---
+++ src/app/auth/signin/page.jsx
@@ -0,0 +1,95 @@
+"use client";
+import React from "react";
+import { Check } from "@gravity-ui/icons";
+import {
+  Button,
+  Description,
+  FieldError,
+  Form,
+  Input,
+  Label,
+  TextField,
+} from "@heroui/react";
+import { authClient } from "@/lib/auth-client";
+
+const page = () => {
+
+    const onSubmit = async (e) => {
+        e.preventDefault();
+    
+        const formData = new FormData(e.currentTarget);
+        const userData = Object.fromEntries(formData.entries())
+        console.log(userData)
+    
+        const { data, error } = await authClient.signIn.email({
+            email: userData.email,
+            password: userData.password,
+            rememberMe: true,
+            callbackURL: "/",
+        });
+
+        console.log(data)
+            
+    };
+
+  return (
+    <div className="w-1/3 mx-auto">
+
+      <Form className="flex w-100 flex-col gap-4" onSubmit={onSubmit}>
+        
+
+        <TextField
+          isRequired
+          name="email"
+          type="email"
+          validate={(value) => {
+            if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+              return "Please enter a valid email address";
+            }
+            return null;
+          }}
+        >
+          <Label>Email</Label>
+          <Input name="email" placeholder="Enter email" />
+          <FieldError />
+        </TextField>
+        <TextField
+          isRequired
+          minLength={8}
+          name="password"
+          type="password"
+          validate={(value) => {
+            if (value.length < 8) {
+              return "Password must be at least 8 characters";
+            }
+            if (!/[A-Z]/.test(value)) {
+              return "Password must contain at least one uppercase letter";
+            }
+            if (!/[0-9]/.test(value)) {
+              return "Password must contain at least one number";
+            }
+            return null;
+          }}
+        >
+          <Label>Password</Label>
+          <Input name="password" placeholder="Enter your password" />
+          <Description>
+            Must be at least 8 characters with 1 uppercase and 1 number
+          </Description>
+          <FieldError />
+        </TextField>
+        <div className="flex gap-2">
+          <Button type="submit">
+            <Check />
+            Submit
+          </Button>
+          <Button type="reset" variant="secondary">
+            Reset
+          </Button>
+        </div>
+      </Form>
+    </div>
+  );
+};
+
+export default page;
